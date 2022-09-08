fe4fd0cf45fe3ebdb9d3ec3aaad5ae822132a5df
nziokikevin <kevinnzioki10@gmail.com>
2022-09-08 12:19:47 +0300

    designing the about component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,39 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 function Footer(){
     return(
-        <div>
+        <section className="bg-dark text-white py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <h6>Company Info</h6>
+                        <hr />
+                        <p>
+                        The broader goals of using a TMS are to improve shipping efficiency, reduce costs, 
+                        gain real-time supply chain visibility and ensure customer satisfaction.
+                        </p>
+                    </div>
 
-        </div>
+                    <div className="col-md-4">
+                        <h6>Quick Links</h6>
+                        <hr />
+                        <div><Link to="/" className="text-decoration-none">Home</Link></div>
+                        <div><Link to="/about" className="text-decoration-none">About Us</Link></div>
+                        <div><Link to="/saccos" className="text-decoration-none">Saccos</Link></div>
+                        <div><Link to="/contact-us" className="text-decoration-none">Contact Us</Link></div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <h6>Contact Information</h6>
+                        <hr />
+                        <div><p>Address: Nairobi, Kenya</p></div>
+                        <div><p>Phone number: 0735120449</p></div>
+                        <div><p>Email: kevinnzioki10@gmail.com</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -24,44 +24,6 @@ function Home() {
             </div>
             {/* carousel section */}
 
-            {/* our company section */}
-            <section className="py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h4>Our Company</h4>
-                            <div className="underline mb-3"></div>
-                            <p>KeNTMS is a logistics and shipping management software that helps businesses streamline operations related to dispatching, accounting, commissions, asset management, and more on a centralized platform. The automated workflow engine uses an IFTTT algorithm to update load details and statuses.</p>
-                            <p>AscendTMS is a logistics and shipping management software that helps businesses streamline operations related to dispatching, accounting, commissions, asset management, and more on a centralized platform. The automated workflow engine uses an IFTTT algorithm to update load details and statuses.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* our company section */}
-
-            {/* mission and vision section */}
-            <section className="mv py-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h4>Our Vision</h4>
-                            <div className="sub-underline mb-3"></div>
-                            <p>
-                                The broader goals of using a TMS are to improve shipping efficiency, reduce costs, gain real-time supply chain visibility and ensure customer satisfaction.
-                            </p>
-                        </div>
-                        <div className="col-md-6">
-                            <h4>Our Mission</h4>
-                            <div className="sub-underline mb-3"></div>
-                            <p>
-                                The broader goals of using a TMS are to improve shipping efficiency, reduce costs, gain real-time supply chain visibility and ensure customer satisfaction.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* mission and vision section */}
-
             {/* services section */}
             <section className="py-5">
                 <div className="container">
@@ -84,7 +46,7 @@ function Home() {
                         <div className="col-md-3">
                             <div className="card shadow">
                                 <div className="card-body">
-                                <h6>Driver Management</h6>
+                                    <h6>Driver Management</h6>
                                     <div className="sub-underline mb-3"></div>
                                     <p>To manage your driver and their activities</p>
                                 </div>
@@ -113,6 +75,7 @@ function Home() {
                     </div>
                 </div>
             </section>
+            {/* services section */}
 
         </div>
     )
